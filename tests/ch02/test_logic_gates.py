301a8c03f21dc4135b08de51e4c992b624b811ab
Vox Dai <vox.dai@icloud.com>
2025-06-13 00:59:34 +0800

    test: Migrate unit tests to pytest

--- tests/ch02/test_logic_gates.py
+++ tests/ch02/test_logic_gates.py
@@ -1,32 +1,29 @@
-import unittest
 from ch02.logic_gates import AND, NAND, OR, XOR
 
 
-class TestLogicGates(unittest.TestCase):
-    def test_and(self):
-        self.assertTrue(AND(1, 1))
-        self.assertFalse(AND(1, 0))
-        self.assertFalse(AND(0, 1))
-        self.assertFalse(AND(0, 0))
-
-    def test_nand(self):
-        self.assertFalse(NAND(1, 1))
-        self.assertTrue(NAND(1, 0))
-        self.assertTrue(NAND(0, 1))
-        self.assertTrue(NAND(0, 0))
-
-    def test_or(self):
-        self.assertTrue(OR(1, 1))
-        self.assertTrue(OR(1, 0))
-        self.assertTrue(OR(0, 1))
-        self.assertFalse(OR(0, 0))
-
-    def test_xor(self):
-        self.assertFalse(XOR(1, 1))
-        self.assertTrue(XOR(1, 0))
-        self.assertTrue(XOR(0, 1))
-        self.assertFalse(XOR(0, 0))
+def test_and():
+    assert AND(1, 1)
+    assert not AND(1, 0)
+    assert not AND(0, 1)
+    assert not AND(0, 0)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_nand():
+    assert not NAND(1, 1)
+    assert NAND(1, 0)
+    assert NAND(0, 1)
+    assert NAND(0, 0)
+
+
+def test_or():
+    assert OR(1, 1)
+    assert OR(1, 0)
+    assert OR(0, 1)
+    assert not OR(0, 0)
+
+
+def test_xor():
+    assert not XOR(1, 1)
+    assert XOR(1, 0)
+    assert XOR(0, 1)
+    assert not XOR(0, 0)
